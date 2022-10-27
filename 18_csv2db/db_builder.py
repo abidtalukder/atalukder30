365a2cc69dbe1e96f5cab717f2388383c429beeb
Abid Talukder <atalukder30@stuy.edu>
2022-10-27 09:10:30 -0400

    lab 18 updates

--- 18_csv2db/db_builder.py
+++ 18_csv2db/db_builder.py
@@ -18,7 +18,7 @@ c = db.cursor()               #facilitate db ops -- you will use cursor to trigg
 
 # < < < INSERT YOUR TEAM'S POPULATE-THE-DB CODE HERE > > >
 
-delete_table ="DROP TABLE IF EXISTS students"
+delete_table ="DROP TABLE IF EXISTS students" # students table, delete if already exists
 c.execute(delete_table)
 create_table = "create table students(name text, age int, id int);"          # test SQL stmt in sqlite3 shell, save as string
 c.execute(create_table)    # run SQL statement
@@ -30,6 +30,18 @@ with open('students.csv') as csv_file:
     for row in csv_reader: 
         c.execute(f'insert into students values(\'{row["name"]}\',{row["age"]},{row["id"]});')
 
+delete_table ="DROP TABLE IF EXISTS courses"
+c.execute(delete_table)
+create_table = "create table courses(code text, mark int, id int);"          
+c.execute(create_table)    # run SQL statement
+
+# Opening CSV file for iteration
+with open('courses.csv') as csv_file:
+    csv_reader = csv.DictReader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader: 
+        c.execute(f'insert into courses values(\'{row["code"]}\',{row["mark"]},{row["id"]});')
+
 #==========================================================
 
 db.commit() #save changes
@@ -39,6 +51,18 @@ students = (c.fetchall())
 print("name | age | id")
 print()
 for group in students:
+    print(f'{group[0]} | {group[1]} | {group[2]}')
+
+print()
+print("**********")
+print()
+
+c.execute("select * from courses;")
+courses = (c.fetchall())
+
+print("code | mark | id")
+print()
+for group in courses:
     print(f'{group[0]} | {group[1]} | {group[2]}')
 
 db.close()  #close database
