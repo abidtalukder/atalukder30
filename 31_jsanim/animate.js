91fc448d04cbad4236f0d145bd2b93dc3c5783d9
Abid Talukder <atalukder30@stuy.edu>
2023-04-25 22:44:52 -0400

    homework 31

--- 31_jsanim/animate.js
+++ 31_jsanim/animate.js
@@ -2,10 +2,6 @@ var c = document.getElementById("playground") // GET CANVAS
 var dotButton = document.getElementById("buttonCircle")
 var stopButton = document.getElementById("buttonStop") //get stop button
 
-// Event listeners
-
-
-
 var ctx = c.getContext("2d") // Your code
 
 ctx.fillStyle = "red"
@@ -22,27 +18,30 @@ var clear = (e) => {
 var radius = 0;
 var growing = true;
 var radius = 10;
+var stopAnimation = false;
+var isDrawing = false;
+
 
 var drawCircle = function(e) {
     if (growing) {
-        if (radius + 5 > height/2){
+        if (radius + 1 > height/2){
             radius = height/2
             growing = false
         } else if (radius === height/2) {
-            radius -= 5
+            radius -= 1
             growing = false
         } else {
-            radius += 5
+            radius += 1
         }
     } else {
-        if (radius - 5 < 10) {
+        if (radius - 1 < 10) {
             radius = 10
             growing = true;
         } else if (radius === 10) {
             growing = true;
-            radius += 5;
+            radius += 1;
         } else {
-            radius -= 5;
+            radius -= 1;
         } 
     }
     
@@ -56,23 +55,53 @@ var drawCircle = function(e) {
     ctx.fill(path) //fills in the result
 };
 
-
+var dotListener = true;
 
 var drawDot = () => {
+    dotButton.removeEventListener("click", drawDot); // you cant click this again once you already clicked it
+    dotListener = false
     
-    
-    //drawCircle()
+    //console.log(requestID-Date.now())
+    //stopAnimation = false
+    //cancelAnimationFrame(drawDot)
     clear()
+    drawCircle()
+    if (!stopAnimation) {
+        requestAnimationFrame(drawDot)
+    } else {
+        stopAnimation = false;
+        dotButton.addEventListener("click", drawDot);
+        dotListener = true
+        
+    }
     
     /*
         Wipe the canvas
         Repaint the circle,
-        
-    
     */
-   window.requestAnimationFrame(drawCircle)
-    
-    
+};
+
+//var stopIt = function ( ) ‹
+var stopIt = () => {
+    console.log("stopIt invoked..");
+    console.log ( requestID );
+    stopAnimation = true;    
+    stopButton.removeEventListener( "click", stopIt);
+    /*YOUR CODE HERE
+    •to stop the animation
+    You will need window. cancelAnimationFrame ()
+    */
+};
+
+var change = () => {
+    if (dotListener) {
+        stopButton.removeEventListener( "click", stopIt); 
+    } else {
+        stopButton.addEventListener( "click", stopIt);
+        stopIt()
+    }
 };
 
 dotButton.addEventListener("click", drawDot);
+stopButton.addEventListener( "click", stopIt);
+stopButton.addEventListener( "click", change);
